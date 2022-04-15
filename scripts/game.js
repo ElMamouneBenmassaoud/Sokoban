ff654adadc7877879cfcf7fa67f3c1798e4742f5
58112@etu.he2b.be <58112@etu.he2b.be>
2022-04-15 06:55:15 +0200

    Étape 2.2 Comprendre l'encodage

--- scripts/game.js
+++ scripts/game.js
@@ -1,1 +1,19 @@
-console.log("hello world");
+"use strict";
+
+/**
+ * Calls the function buildLevel once the page is loaded
+ */
+window.addEventListener("load", function(event) {
+    buildLevel(0);
+});
+
+/**
+* Output the level in the console
+* @param {number} level The level's number
+*/
+function buildLevel(level) {
+    for (let i=0; i<levels[level].map.length; i++) {
+        console.log(levels[level].map[i]);
+    }
+    
+}
