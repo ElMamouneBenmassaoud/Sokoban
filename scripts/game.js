40ade8a2c0ed29b66601ff1309d01bb184533fec
58112@etu.he2b.be <58112@etu.he2b.be>
2022-04-15 07:55:28 +0200

    Étape 2.4 : Intégration d’un niveau

--- scripts/game.js
+++ scripts/game.js
@@ -12,8 +12,47 @@ window.addEventListener("load", function(event) {
 * @param {number} level The level's number
 */
 function buildLevel(level) {
+    // Loop over all lines of the level map
     for (let i=0; i<levels[level].map.length; i++) {
-        console.log(levels[level].map[i]);
+        let line = levels[level].map[i];
+        // Creates the div representing a line
+        let lineDiv = document.createElement("div");
+        // Adds the class "line" to the line div
+        $(lineDiv).addClass("line");
+        // Appends the line div to the #world div
+        $("#world").append(lineDiv);
+        // Loop over all the symbols in the line
+        for (const symbol of line) {
+            // Creates the div representing a square
+            let squareDiv = document.createElement("div");
+            // Adds the "square" class to the square div
+            $(squareDiv).addClass("square");
+
+            // Adds the correct class depending on the symbol
+            switch (symbol) {
+                case ' ': 
+                $(squareDiv).addClass("floor");
+                break;
+                case 'x': 
+                $(squareDiv).addClass("target");
+                break;
+                case '🧍': 
+                $(squareDiv).addClass("player");
+                break;
+                case '#': 
+                $(squareDiv).addClass("box");
+                break;
+                case '@': 
+                $(squareDiv).addClass("boxOnTarget");
+                break;
+                default: 
+                $(squareDiv).addClass("wall");
+                break;
+            }
+
+            //Appends the square div to the line div
+            lineDiv.append(squareDiv);
+        }
     }
     
 }
