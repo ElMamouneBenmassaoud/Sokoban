a59e18a219309d7561bbcbc672157ed1ce305fc6
58112@etu.he2b.be <58112@etu.he2b.be>
2022-05-05 09:44:42 +0200

    4.6 : Recommencer un niveau

--- scripts/game.js
+++ scripts/game.js
@@ -15,6 +15,10 @@ window.addEventListener("load", function (event) {
         move(events);
         finishLevel(events);
     });
+    // @ts-ignore
+    document.getElementById("restart").addEventListener("click", function() {
+        initLevel(currentLevel);
+    });
 });
 
 /**
